1ee8cc26472ce8e32dff469f81e3efba1c0df25d
Umair-Zubair <umair123zubair@gmail.com>
2024-11-04 01:18:41 +0500

    bottle.css

--- React-check/product-showcase/src/pages/Bottle.jsx
+++ React-check/product-showcase/src/pages/Bottle.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import ColorChangingSpline from '../components/ColorChangingSpline';
+import '../bottle.css';
 
 const BottlePage = () => {
   return (
